024d85eee24f36437351eea2ff9fc0aa16145355
miri-kk <m0556780173@gmail.com>
2024-07-30 20:48:09 +0300

    change2 port

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import appController from './/controllers/appController.js';
 import express from "express";
 import cors from "cors";
 
-const app = express()
+const port = 8080;
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -18,8 +19,8 @@ const secret = "JIs%WCfS#Sl454d5FX";
 
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on http://localhost:8080`)
+app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port}`)
   })
 
 console.log("hi")
