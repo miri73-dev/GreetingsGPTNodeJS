d3083ec08f6fe8e170ceb14aca6c56e491788950
miri-kk <m0556780173@gmail.com>
2024-08-06 11:51:00 +0300

    delete ENV

--- controllers/appController.js
+++ controllers/appController.js
@@ -5,7 +5,7 @@ dotenv.config();
 const secret = "JIs%WCfS#Sl454d5FX";
 
 const openai = new OpenAI ({
-    apiKey:OPENAI_API_KEY,    
+    apiKey:process.env.OPENAI_API_KEY,    
 });
 
 const appController={
